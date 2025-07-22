70843d9f1203552b0cc3943c1ada888656c5efc3
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-07-23 00:06:15 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
 BOT_NAME = "Auto"
-BOT_TOKEN = ""
+BOT_TOKEN = "7293892972:AAFO1TIZxXVyGb_VyT8EPMgZfhXEcMK0dvQ"
 API_ID = "27765349"
 API_HASH = "9df1f705c8047ac0d723b29069a1332b"
 MONGO_URI = "mongodb+srv://staraj67890:HvA9VVkHVp7Jzdox@cluster0.jhpzxdh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
